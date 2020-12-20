6541cf55b10a8f3b6f669ada8daf7b0832b7e891
Mariia Bondar <param.pam.pam.mariia@gmail.com>
2020-12-20 23:24:35 +0200

    task -1 done

--- task-1.js
+++ task-1.js
@@ -3,3 +3,12 @@
 
 // Нумерация должна начинаться с 1. К примеру для первого элемента массива ['Mango', 'Poly', 'Ajax'] 
 //с индексом 0 будет выведено '1 - Mango', а для индекса 2 выведет '3 - Ajax'.
+
+const logItems = (array) => {
+for (let index = 0; index < array.length; index++) {
+    const element = array.indexOf(array[index]) + 1;
+    console.log(`${element} - ${array[index]}`);
+}   
+}
+logItems(['Mango', 'Poly', 'Ajax']);
+logItems(['apple', 'pear', 'orange', 'banana']);
